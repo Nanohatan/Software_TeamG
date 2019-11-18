f4582a70be0c1e0db135044c8cc82529a01c2df9
Nonoka Homma <e185730@ie.u-ryukyu.ac.jp>
2019-11-18 21:45:53 +0900

    add style seet

--- loginDemo/LoginDemo/LoginDemo/ViewController/LoginViewController.swift
+++ loginDemo/LoginDemo/LoginDemo/ViewController/LoginViewController.swift
@@ -9,22 +9,30 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    func  setUpElements(){
+        errorLabel.alpha = 0
+        
+        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleFilledButton(loginButton)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginTapped(_ sender: Any) {
     }
-    */
-
+    
 }
--- loginDemo/LoginDemo/LoginDemo/ViewController/SignUpViewController.swift
+++ loginDemo/LoginDemo/LoginDemo/ViewController/SignUpViewController.swift
@@ -9,22 +9,37 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passworftextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setUpElements()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpElements(){
+        errorLabel.alpha = 0
+        
+        Utilities.styleTextField(lastNameTextField)
+        Utilities.styleTextField(firstNameTextField)
+        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(passworftextField)
+        Utilities.styleFilledButton(signUpButton)
     }
-    */
 
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+    
 }
