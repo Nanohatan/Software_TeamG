a54caa879739d993a35d881b3dc2741387802269
Nonoka Homma <e185730@ie.u-ryukyu.ac.jp>
2019-12-19 11:48:09 +0900

    delet task struct

---
+++ loginDemo/LoginDemo/LoginDemo/Task.swift
@@ -0,0 +1,18 @@
+//
+//  Task.swift
+//  LoginDemo
+//
+//  Created by 本間ののか on 2019/12/18.
+//  Copyright © 2019 本間ののか. All rights reserved.
+//
+
+import Foundation
+struct Task {
+    var name:String
+    var date:String
+    
+    init(name:String, date:String){
+        self.name = name
+        self.date = date
+    }
+}
